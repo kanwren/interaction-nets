a8aed5f045416b35f21d92cb5429f0e3fd4cb7a2
Elton <eltonp3103@gmail.com>
2022-04-19 16:55:40 -0400

    Define substitution

--- theory/Inet.v
+++ theory/Inet.v
@@ -5,6 +5,7 @@ Import List.ListNotations.
 From Inet Require Import Map.
 
 Open Scope string_scope.
+Open Scope list_scope.
 
 (* Default notation for vectors overrides list notation. Redefine it *)
 Notation "'<<' '>>'" := (Vector.nil _) (at level 60, right associativity).
@@ -42,14 +43,40 @@ Fail Check tree 1 "alpha" << leaf "beta"; leaf "gamma" >>.
 Fixpoint names_of_term (t : term) : list string :=
   match t with
   | leaf n => [n]
-  | tree arity _ terms => 
+  | tree _ _ terms => 
       List.concat (Vector.to_list (Vector.map names_of_term terms))
   end.
 
 Compute names_of_term (leaf "alpha").
 Compute names_of_term (tree 2 "alpha" << leaf "beta"; leaf "gamma" >>).
 
-(* TODO Renaming and Substitution *)
+(* Renaming and Substitution *)
+Reserved Notation "'[' x ':=' u ']' t" (at level 60).
+Fixpoint subst (t : term) (x : string) (u : term) : term :=
+  match t with
+  | leaf n =>
+      if eqb_string n x then
+        u
+      else
+        t
+  | tree arity agent terms =>
+      tree arity agent (Vector.map (fun t =>
+        [x := u] t
+      ) terms)
+  end
+where "'[' x ':=' u ']' t" := (subst t x u).
+
+Compute ["x" := leaf "y"] (leaf "x").
+Compute ["x" := tree 2 "x" << leaf "y"; leaf "z" >>] (
+  tree 2 "alpha" <<
+    leaf "x";
+    tree 3 "beta" <<
+      leaf "x";
+      leaf "y";
+      leaf "z"
+    >>
+  >>
+).
 
 (* Rules *)
 Axiom rule : relation term.
@@ -59,6 +86,10 @@ Notation "t1 '⋈' t2" := (interacts_with t1 t2) (at level 60, right associativi
 
 Check (leaf "alpha") ⋈ (leaf "beta").
 Check (tree 2 "alpha" << leaf "beta"; leaf "gamma" >>) ⋈ (leaf "beta").
+Check [ (leaf "alpha") ⋈ (leaf "beta");
+        (tree 2 "alpha" << leaf "beta"; leaf "gamma" >>) ⋈ (leaf "beta")
+      ].
+  
 
 (* Equations *)
 Definition eqn (X : Type) : Type := X -> X -> Prop.
